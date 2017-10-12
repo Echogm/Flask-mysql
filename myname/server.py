514e45b95171aeac501d52f3fd057366d2373c91
echogm <silverk2112@gmail.com>
2017-10-12 16:57:30 -0400

    V0.1

--- myname/server.py
+++ myname/server.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key = "i am super secret"
+x = []
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    return render_template("index.html", x = x)
 @app.route("/process", methods=["POST"])
 def process():
-    names = []
-    session["results"] = request.form
-    names.append("names")
+    # print request.form["name"]
+    x.append(request.form["name"])
     return redirect("/")
 app.run(debug=True)
