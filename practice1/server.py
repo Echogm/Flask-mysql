9fb135f999642c818c2fc464e2878c41e8db05bd
echogm <silverk2112@gmail.com>
2017-10-13 18:07:11 -0400

    Working on this

---
+++ practice1/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, session
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+app.secret_key = "this is supper secret"
+
+db = MySQLConnector(app,"songs_artist")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/songs/new", methods=["POST"])
+def createSong():
+    query = "INSERT INTO songs(name,popularity) VALUES(:name,:popularity)"
+    data = {
+        "name":request.form["name"],
+        "popularity":request.form["popularity"]
+    }
+    db.query_db(query,data)
+    return redirect("/")
+
+@app.route("/artists/new",methods=["POST"])
+def createArtists(arg):
+
+    return redirect("/")
+
+
+app.run(debug=True)
