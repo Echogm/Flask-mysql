2023352dfd4b150a0ab17c600ff62df55a3f58ee
echogm <silverk2112@gmail.com>
2017-10-17 18:31:16 -0400

    Cleaning Files

--- desappearingninja2.0/server.py
+++ desappearingninja2.0/server.py
@@ -2,38 +2,16 @@ from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
 
-# flag = False
-# def check(x):
-#     if x != "blue" or "red" or "purple" or "orange" or "ninja":
-#          flag = True
-#     else:
-#          flag = False
-#     return flag
-
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/ninja")
 def ninja():
     return render_template("ninja.html")
-# @app.route("/ninja/blue")
-# def blue():
-#     return render_template("blue.html")
-# @app.route("/ninja/orange")
-# def orange():
-#     return render_template("orange.html")
-# @app.route("/ninja/red")
-# def red():
-#     return render_template("red.html")
-# @app.route("/ninja/purple")
-# def purple():
-#     return render_template("purple.html")
+
 @app.route("/ninja/<x>")
 def check(x):
     x = str(x)
-
-    # if x != "blue" or "red" or "purple" or "orange":
-        # return redirect("/notapril")
 
     if x == "blue":
         return render_template("blue.html");
