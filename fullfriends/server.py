89604662ffec3df9679ddb7763a2eb9e9ad64a32
echogm <silverk2112@gmail.com>
2017-10-16 23:51:23 -0400

    first commit

--- fullfriends/server.py
+++ fullfriends/server.py
@@ -8,14 +8,13 @@ db = MySQLConnector(app, "friends")
 @app.route("/")
 def index():
     friends = db.query("SELECT * FROM friends;")
-    # name = request.form("name")
-    # age = request.form("age")
-    return render_template("index.html", friends=friends)
+
+    return render_template("index.html", friends = friends)
 
 @app.route("/addfriend", methods=["POST"])
 def create():
 
-    query = "INSERT INTO friends(name, age, createdAt, updatedAt) VALUES (:name, :age, :NOW(), :NOW());"
+    query = "INSERT INTO friends(name, age, createdAt, updatedAt) VALUES (:name, :age, NOW(), NOW());"
 
     data = {
         "name":request.form["name"],
